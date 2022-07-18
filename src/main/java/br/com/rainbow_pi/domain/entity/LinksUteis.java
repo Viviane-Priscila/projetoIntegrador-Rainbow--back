4f6d248b3e9226ca0b070522a20e23689feb7115
nligabo <nataliamligabo@gmail.com>
2022-07-18 18:14:55 -0300

    Atualizando projeto

--- src/main/java/br/com/rainbow_pi/domain/entity/LinksUteis.java
+++ src/main/java/br/com/rainbow_pi/domain/entity/LinksUteis.java
@@ -1,5 +1,31 @@
 package br.com.rainbow_pi.domain.entity;
 
-public class LinksUteis {
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="tb_links")
+@Data
+@NoArgsConstructor
+public class LinksUteis implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@Column(name="id_link")
+	private Long id;
+	
+	@Column(name="conteudo_link")
+	private String conteudo;
+	
+	@Column(name="url_link")
+	private String url;
 
 }
--- src/main/java/br/com/rainbow_pi/domain/entity/Profissional.java
+++ src/main/java/br/com/rainbow_pi/domain/entity/Profissional.java
@@ -1,7 +1,30 @@
 package br.com.rainbow_pi.domain.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Entity
+@Table(name="tb_profissional")
+@NoArgsConstructor
+@Getter
+@Setter
 public class Profissional extends Pessoa{
 
 	private static final long serialVersionUID = 1L;
+	
+	@Column(name="id_profissional")
+	private Long idProfissional;
+	
+	@Column(name="profissao_profissional")
+	private String profissao;
+	
+	@Column(name="registro_profissional")
+	private String registroProfissional;
 
 }
