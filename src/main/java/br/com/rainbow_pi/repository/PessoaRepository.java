b3089609f71bfa265a46c29a73ac630e98a3d99e
nligabo <nataliamligabo@gmail.com>
2022-07-18 17:42:13 -0300

    Atualização

--- src/main/java/br/com/rainbow_pi/repository/PessoaRepository.java
+++ src/main/java/br/com/rainbow_pi/repository/PessoaRepository.java
@@ -1,6 +1,5 @@
 package br.com.rainbow_pi.repository;
 
-<<<<<<< HEAD
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,11 +7,6 @@ import br.com.rainbow_pi.domain.entity.Pessoa;
 
 @Repository
 public interface PessoaRepository extends JpaRepository<Pessoa, Long>{
-=======
-import org.springframework.stereotype.Repository;
 
-@Repository
-public interface PessoaRepository {
->>>>>>> b5c1a94dbd63c31a7eb98c2c176fc5c019d2ffba
 
 }
